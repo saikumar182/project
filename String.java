467d4bc94b240d8c75be4e6ae2f269f9381816ff
saikumar182 <129929094+saikumar182@users.noreply.github.com>
2023-04-06 09:14:12 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,19 @@
+package library;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+	     
+		String s1 ="java";
+		
+		System.out.println (s1.charAt(2));
+		
+
+	}
+
+	private char[] charAt(int i) {
+		
+		return null;
+	}
+
+}
